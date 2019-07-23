4b3c9f95ff8793ef9169d4ada200d2f35a90a6c0
Peter Huang <pete_huang88@yahoo.com>
2019-07-23 15:36:22 -0400

    Fixed slow fast to take in frames rather than video + trained on 3 classes only

--- sort_tracker/SlowFast/test.py
+++ sort_tracker/SlowFast/test.py
@@ -7,8 +7,38 @@ from lib.dataset import VideoDataset
 from lib import slowfastnet
 import torchvision
 import cv2
+import os
 # put parameters of slowfast into config.py file
 from config import params
+
+def read_frames(filename):
+    root = "/local/b/cam2/data/HumanBehavior/2"
+    lst = open(filename, "r")
+    file_list = []
+    count = 0
+    for f in lst:
+        full = os.path.join(root, f)
+        full = full.rstrip()
+        # print(full)
+        file_list.append(full)
+        if count >= 32:
+            break
+        count += 1
+    
+    buff = np.empty((1,len(file_list), 112, 112, 3), np.dtype('float32'))
+    count = 0
+
+    for full in file_list:
+        img = cv2.imread(full)
+        img = cv2.resize(img, (112,112))
+        img = (img - 128.0)/128.0
+
+        buff[0, count] = img
+        count += 1
+                
+    buff = buff.transpose((0,4,1,2,3))
+        
+    return torch.from_numpy(buff)
 
 def load_vid(filename):
     capture = cv2.VideoCapture(filename)
@@ -18,7 +48,9 @@ def load_vid(filename):
     ret = True
     while (count < frame_count and ret):
         ret, frame = capture.read()
-   
+        print(frame.shape)
+        if not ret:
+            continue 
         # get frame, resize
         new_frame = cv2.resize(frame, (112,112))  
         
@@ -63,19 +95,43 @@ def main():
     model = nn.DataParallel(model, device_ids=params['gpu'])
 
     # TODO: load a dataset
+    tn = read_frames("names.txt")
+
+    model.eval()
+    output = model(tn)
+    prediction = torch.argmax(output)
+    print(output)
+    print(prediction)
+    
 
 
     # /local/b/cam2/data/HumanBehavior/slowfasttest/v_Biking_g09_c01.avi
+    """
     testset = '/local/b/cam2/data/HumanBehavior/UCFsample'
-    
+    testset2 = '/local/b/cam2/data/HumanBehavior/UCFsample/validation/biking'
+    video_files = []
+    for r, d, f in os.walk(testset2):
+        for file in f:
+            if '.avi' in file:
+                video_files.append(os.path.join(r, file))
 
+    model.eval()
+    for vid_file in video_files:
+        data = load_vid(vid_file)
+        output = model(data)
+        prediction = torch.argmax(output)
+        print(prediction)  
+
+    """
+                 
     # data = torch.randn(1,3,10,112,112)
     
     # data = load_vid('/local/b/cam2/data/HumanBehavior/slowfasttest/v_Biking_g04_c01.avi')
-    data = DataLoader(VideoDataset(testset, mode='validation', clip_len=64,frame_sample_rate=1),batch_size=5, shuffle=False,num_workers=4)
+    """
+    # data = DataLoader(VideoDataset(testset, mode='validation', clip_len=64,frame_sample_rate=1),batch_size=5, shuffle=False,num_workers=4)
     # print(data.shape)
     
-    model.eval()
+     model.eval()
     for step, (inputs, label) in enumerate(data):
         # print(inputs.shape)
         output = model(inputs)
@@ -83,7 +139,7 @@ def main():
     
         if step > 5:
             break
-
+    """
     
     # get output from model
     #output = model(data)
